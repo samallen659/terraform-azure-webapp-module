633a87463ccc5ddfe2908c1f848056420be4ba65
Sam Allen <samallen659@gmail.com>
2026-01-16 19:48:33 +0000

    adds network config

---
+++ network.tf
@@ -0,0 +1,84 @@
+resource "azurerm_virtual_network" "vnet-main" {
+  name                = "vnet-${var.project_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-main.name
+  address_space       = ["10.0.0.0/16"]
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+    security_group   = azurerm_network_security_group.nsg-main.id
+  }
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+    security_group   = azurerm_network_security_group.nsg-main.id
+  }
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_network_security_group" "nsg-main" {
+  name                = "nsg-${var.project_name}"
+  resource_group_name = azurerm_resource_group.rg-main.name
+  location            = var.location
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_network_security_rule" "rule-web-http" {
+  name                        = "AllowHTTP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "10.0.1.0/24"
+  resource_group_name         = azurerm_resource_group.rg-main.name
+  network_security_group_name = azurerm_network_security_group.nsg-main.name
+}
+
+resource "azurerm_network_security_rule" "rule-web-https" {
+  name                        = "AllowHTTPS"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "10.0.1.0/24"
+  resource_group_name         = azurerm_resource_group.rg-main.name
+  network_security_group_name = azurerm_network_security_group.nsg-main.name
+}
+
+resource "azurerm_network_security_rule" "rule-db-5432" {
+  name                        = "AllowPostgesql"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5432"
+  source_address_prefix       = "10.0.1.0/24"
+  destination_address_prefix  = "10.0.2.0/24"
+  resource_group_name         = azurerm_resource_group.rg-main.name
+  network_security_group_name = azurerm_network_security_group.nsg-main.name
+}
+
+resource "azurerm_network_security_rule" "rule-db-block" {
+  name                        = "BlockALL"
+  priority                    = 103
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "10.0.2.0/24"
+  resource_group_name         = azurerm_resource_group.rg-main.name
+  network_security_group_name = azurerm_network_security_group.nsg-main.name
+}

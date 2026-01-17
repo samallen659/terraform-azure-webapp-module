c94d4f7deb3ca495c01e4f420a34121357b8a228
Sam Allen <samallen659@gmail.com>
2026-01-17 14:06:02 +0000

    adds loadbalancer config

--- compute.tf
+++ compute.tf
@@ -26,13 +26,63 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "vmss-main" {
     name    = "vmss-nic"
     primary = true
     ip_configuration {
-      name      = "internal"
-      primary   = true
-      subnet_id = azurerm_subnet.subnet-web.id
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet-web.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bep-main.id]
     }
   }
   upgrade_mode = "Manual"
   tags = {
     environment = var.environment
   }
+}
+
+resource "azurerm_public_ip" "pip-main" {
+  name                = "pip-lbe-${var.project_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-main.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "lbe-main" {
+  name                = "lbe-${var.project_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-main.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIpAddress"
+    public_ip_address_id = azurerm_public_ip.pip-main.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bep-main" {
+  name            = "bep-${var.project_name}"
+  loadbalancer_id = azurerm_lb.lbe-main.id
+}
+
+resource "azurerm_lb_rule" "lbr-http" {
+  loadbalancer_id                = azurerm_lb.lbe-main.id
+  name                           = "lbr-http"
+  protocol                       = "Tcp"
+  frontend_port                  = "80"
+  backend_port                   = "80"
+  frontend_ip_configuration_name = "PublicIpAddress"
+  probe_id                       = azurerm_lb_probe.lprobe-main.id
+}
+
+resource "azurerm_lb_rule" "lbr-https" {
+  loadbalancer_id                = azurerm_lb.lbe-main.id
+  name                           = "lbr-https"
+  protocol                       = "Tcp"
+  frontend_port                  = "443"
+  backend_port                   = "443"
+  frontend_ip_configuration_name = "PublicIpAddress"
+  probe_id                       = azurerm_lb_probe.lprobe-main.id
+}
+
+resource "azurerm_lb_probe" "lprobe-main" {
+  loadbalancer_id = azurerm_lb.lbe-main.id
+  name            = "http-running-probe"
+  port            = 80
 }

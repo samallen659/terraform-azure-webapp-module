8ec52c1155367bb2a34ed6229c2ee7c7b41e44bd
Sam Allen <samallen659@gmail.com>
2026-01-17 15:35:15 +0000

    adds bastion

--- network.tf
+++ network.tf
@@ -22,6 +22,33 @@ resource "azurerm_subnet" "subnet-db" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_subnet" "subnet-bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg-main.name
+  virtual_network_name = azurerm_virtual_network.vnet-main.name
+  address_prefixes     = ["10.0.255.0/24"]
+}
+
+resource "azurerm_public_ip" "pip-bas-main" {
+  name                = "pip-bas-${var.project_name}"
+  resource_group_name = azurerm_resource_group.rg-main.name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bas-main" {
+  name                = "bas-${var.project_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-main.name
+  ip_configuration {
+    name                 = "ip_configuration"
+    subnet_id            = azurerm_subnet.subnet-bastion.id
+    public_ip_address_id = azurerm_public_ip.pip-bas-main.id
+  }
+  sku = "Basic"
+}
+
 resource "azurerm_network_security_group" "nsg-main" {
   name                = "nsg-${var.project_name}"
   resource_group_name = azurerm_resource_group.rg-main.name
@@ -85,4 +112,14 @@ resource "azurerm_network_security_rule" "rule-db-block" {
   destination_address_prefix  = "10.0.2.0/24"
   resource_group_name         = azurerm_resource_group.rg-main.name
   network_security_group_name = azurerm_network_security_group.nsg-main.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = azurerm_subnet.subnet-web.id
+  network_security_group_id = azurerm_network_security_group.nsg-main.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "db" {
+  subnet_id                 = azurerm_subnet.subnet-db.id
+  network_security_group_id = azurerm_network_security_group.nsg-main.id
 }
